2bb1d872a5ab41b2e7974b96e6f9168c9a9fdbc8
Elin Blomgren <elinblomgren@gmail.com>
2016-03-08 11:21:06 +0100

    add testdata to see score of models

--- 1DimPolynom.py
+++ 1DimPolynom.py
@@ -6,23 +6,39 @@ from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 
+
 #data = None
 
-with open('test.json') as jsonfile:
+with open('training.json') as jsonfile:
     points = json.load(jsonfile)['points']
+    jsonfile.close()
 
 X = []
 y = []
 for point in points:
-    X.append(point['tid'])
+    X.append(point['time'])
     y.append(point['count'])
 
 X = np.asarray(X)
 y = np.asarray(y)
 
+with open('test.json') as testfile:
+    testPoints = json.load(testfile)['points']
+    testfile.close()
+X_test = []
+y_test = []
+for point in testPoints:
+    X_test.append(point['time'])
+    y_test.append(point['count'])
+
+X_test = np.asarray(X_test)
+y_test = np.asarray(y_test)
+
+
 x_plot = np.linspace(0, 1440, 100)
 X = X[:, np.newaxis]
 X_plot = x_plot[:, np.newaxis]
+X_test = X_test[:, np.newaxis]
 
 
 
@@ -37,23 +53,15 @@ print X.shape
 '''
 
 plt.scatter(X, y, label="training points")
+plt.scatter(X_test, y_test, label="test points", c='g')
 
 for degree in [3, 4, 5]:
     model = make_pipeline(PolynomialFeatures(degree), Ridge())
     model.fit(X, y)
+    print model.score(X_test, y_test)
     y_plot = model.predict(X_plot)
     plt.plot(x_plot, y_plot, label="degree %d" % degree)
 
 plt.legend(loc='upper left')
 
-#plt.show()
-
-
-from sklearn.svm import SVR
-
-# # Fit regression model
-svr_rbf = SVR(kernel='rbf', degree=3, C=1e3)
-y_rbf = svr_rbf.fit(X, y).predict(X)
-plt.hold('on')
-plt.plot(X, y_rbf, c='g', label='RBF model')
 plt.show()
--- loadData.py
+++ loadData.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from elasticsearch import Elasticsearch
 import numpy as np
 import json
@@ -8,73 +9,87 @@ client = Elasticsearch(elastic)
 
 response = client.search(
     index="*",
-    body=
-{
-  "size": 0,
-  "query": {
-    "filtered": {
+    body={
+      "size": 0,
       "query": {
-        "query_string": {
-          "query": "*",
-          "analyze_wildcard": True
+        "filtered": {
+          "query": {
+            "query_string": {
+              "analyze_wildcard": True,
+              "query": "*"
+            }
+          },
+          "filter": {
+            "bool": {
+              "must": [
+                {
+                  "range": {
+                    "VisitRegistrationTime": {
+                      "gte": 1457046000000,
+                      "lte": 1457391600000,
+                      "format": "epoch_millis"
+                    }
+                  }
+                }
+              ],
+              "must_not": []
+            }
+          }
         }
       },
-      "filter": {
-        "bool": {
-          "must": [
-            {
-              "range": {
-                "VisitRegistrationTime": {
-                  "gte": 1457218800000,
-                  "lte": 1457305200000,
-                  "format": "epoch_millis"
-                }
-              }
+      "aggs": {
+        "2": {
+          "date_histogram": {
+            "field": "VisitRegistrationTime",
+            "interval": "1h",
+            "time_zone": "Europe/Berlin",
+            "min_doc_count": 1,
+            "extended_bounds": {
+              "min": 1457046000000,
+              "max": 1457391600000
             }
-          ],
-          "must_not": []
+          }
         }
       }
     }
-  },
-  "aggs": {
-    "2": {
-      "date_histogram": {
-        "field": "VisitRegistrationTime",
-        "interval": "1h",
-        "time_zone": "Europe/Berlin",
-        "min_doc_count": 1,
-        "extended_bounds": {
-          "min": 1457218800000,
-          "max": 1457305200000
-        }
-      }
-    }
-  }
-}
 )
 
 
 print(response['hits']['total'])
 x_t = []
 y_t = []
+tx_t = []
+ty_t = []
 count = 0
-avg = 0
 for hit in response['aggregations']['2']['buckets']:
-    #avg = hit['1']['value']
-    #if(avg is not None):
-        time = hit['key_as_string'][11:16]
-        count = hit['doc_count']
+    time = hit['key_as_string'][11:16]
+    count = hit['doc_count']
+    if(hit['key_as_string'][9:10]!="7"):
         x_t.append(int(time[:-3])*60+int(time[-2:]))
         y_t.append(count)
+    else:
+        tx_t.append(int(time[:-3])*60+int(time[-2:]))
+        ty_t.append(count)
 
+
+# training data
 points = []
-
 for i in np.arange(len(x_t)):
-    points.append({'tid': x_t[i], 'count': y_t[i]})
+    points.append({'time': x_t[i], 'count': y_t[i]})
 
 data = {'points': points}
 
-with file('test.json', 'w') as file:
-    json.dump(data, file)
-    file.close()
+with file('training.json', 'w') as f1:
+    json.dump(data, f1)
+    f1.close()
+
+# test data
+points = []
+for i in np.arange(len(tx_t)):
+    points.append({'time': tx_t[i], 'count': ty_t[i]})
+
+data = {'points': points}
+
+with file('test.json', 'w') as f2:
+    json.dump(data, f2)
+    f2.close()
