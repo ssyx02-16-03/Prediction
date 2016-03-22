e82852deb53b4e55219a9a4dd732f417e5126dae
Edvard <edvardlindelof@gmail.com>
2016-03-22 14:36:33 +0100

    småändringar

--- predictions/FourierizedLinear/predict.py
+++ predictions/FourierizedLinear/predict.py
@@ -6,6 +6,8 @@ data = np.load("data.npz")
 times = data["times"]
 ttt = data["ttt"]
 
+r_and_o = data["reds"] + data["oranges"]
+
 # Fouriertermer, antagen periodicitet en vecka
 week_millis = 1000 * 60 * 60 * 24 * 7
 x1 = np.cos((2 * np.pi / week_millis) * 1 * times)
@@ -17,11 +19,12 @@ x6 = np.cos((2 * np.pi / week_millis) * 6 * times)
 x7 = np.cos((2 * np.pi / week_millis) * 7 * times)
 
 X = np.column_stack([x1, x2, x3, x4, x5, x6, x7])
-y = ttt
+y = r_and_o
 
 regr = linear_model.LinearRegression()
 regr.fit(X, y)
 c = regr.coef_
+print c
 
 ypred = c[0] * np.cos((2 * np.pi / week_millis) * 1 * times) +\
     c[1] * np.cos((2 * np.pi / week_millis) * 2 * times) +\
@@ -31,6 +34,6 @@ ypred = c[0] * np.cos((2 * np.pi / week_millis) * 1 * times) +\
     c[5] * np.cos((2 * np.pi / week_millis) * 6 * times) +\
     c[6] * np.cos((2 * np.pi / week_millis) * 7 * times)
 
-plt.plot(times, ttt, 'ro')
-plt.plot(times, ypred * 5 + 1000000)
+plt.plot(times, r_and_o, 'ro')
+plt.plot(times, ypred)
 plt.show()
