8dfd61a043bf2a9bf0ae3376d3451dcaeabd935b
marptt <laxenmeflaxen+git@gmail.com>
2016-03-23 17:29:49 +0100

    Remember when i told everyone that the date parsing was fine? it was not fine. It should be fine now

--- elastic_api/parse_date.py
+++ elastic_api/parse_date.py
@@ -1,21 +1,31 @@
 import time
+import calendar
 
 
 def date_to_millis(date):
     """
     Transforms a date to epoch-millis.
 
-    Since the database uses three (or maybe four) different time formats and i did not want to fix that this function
-    was needed. If you manage to crash this with a new date format, please append it to the list
+    Since the database uses three (or maybe four) different time formats and i did not want to fix that, this function
+    was needed. If you manage to crash this with a new date format, please append it to the list.
+
+    Time zones are handled manually because it was convenient and i have no shame
     """
-    formats = [u"%Y-%m-%dT%H:%M:%SZ", u"%Y-%m-%dT%H:%M:%S.000+0200", u"%Y-%m-%dT%H:%M:%S.000+0100", u"%Y-%m-%d %H:%M"]
-    for format_string in formats:
+
+    ONE_HOUR_IN_SECONDS = 60 * 60
+    try:
+        return long(calendar.timegm(time.strptime(date, u"%Y-%m-%dT%H:%M:%SZ"))) * 1000
+    except ValueError:
         try:
-            return long(time.mktime(time.strptime(date, format_string))) * 1000
+            return long(calendar.timegm(time.strptime(date, u"%Y-%m-%dT%H:%M:%S.000+0100")) - ONE_HOUR_IN_SECONDS) * 1000
         except ValueError:
-            pass  # do nothing, try the next format in the list
+            try:
+                return long(calendar.timegm(time.strptime(date,  u"%Y-%m-%dT%H:%M:%S.000+0200")) - 2 * ONE_HOUR_IN_SECONDS) * 1000
+            except ValueError:
+                try:
+                    return long(calendar.timegm(time.strptime(date, u"%Y-%m-%d %H:%M"))) * 1000
+                except ValueError:
+                    pass  # do nothing
 
-    print "unrecognized format"
+    print "unrecognized date format: " + date
     raise ValueError
-
-
