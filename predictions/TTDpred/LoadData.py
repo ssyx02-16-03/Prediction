08358dc9501af0dc51c16ecd264f9a1cec4de64a
Elin Blomgren <elinblomgren@gmail.com>
2016-03-31 10:54:20 +0200

    added more parameters to TTD prediction

--- predictions/TTDpred/LoadData.py
+++ predictions/TTDpred/LoadData.py
@@ -8,7 +8,7 @@ from elastic_api.NewPatientsLoader import NewPatientsLoader
 from elastic_api.OngoingsLoader import OngoingsLoader
 from elastic_api.TimeToEventLoader import TimeToEventLoader
 from elastic_api.WaitTimeLoaders import TTTLoader, TTDLoader
-from elastic_api.ColorLoaders import RedsLoader
+from elastic_api.ColorLoaders import RedsLoader, OrangesLoader, YellowsLoader, GreensLoader
 
 start_time = "2016-03-09 00:00"
 end_time = "2016-03-11 00:00"
@@ -23,6 +23,9 @@ mep = OngoingsLoader(start_time, end_time, interval)
 avgWait = AverageTimeWaitedLoader(start_time, end_time, interval)
 avgWait.set_search_doctor()
 red = RedsLoader(start_time, end_time, interval)
+orange = OrangesLoader(start_time, end_time, interval)
+yellow = YellowsLoader(start_time, end_time, interval)
+green = GreensLoader(start_time, end_time, interval)
 
 mep.set_match({"filtered" : {
     "filter": {
@@ -56,8 +59,11 @@ x5 = mep.load_vector()
 #x6 = ongoing.get_weekdays()
 x6 = avgWait.load_vector()
 x7 = red.load_vector()
+x8 = orange.load_vector()
+x9 = yellow.load_vector()
+x10 = green.load_vector()
 
-X = np.column_stack([x1, x2, x3, x4, x5, x6, x7])
+X = np.column_stack([x1, x2, x3, x4, x5, x6, x7, x8, x9, x10])
 y = futureTTD.load_vector()
 
 print X
