de5b323246e3e82fdcebc4da4b1b31d1f2e6ad81
Edvard <edvardlindelof@gmail.com>
2016-04-27 19:48:35 +0200

    ttd-week-pattern-predictor now uses LWRegressor

--- predictions/TTDWeek/predict.py
+++ predictions/TTDWeek/predict.py
@@ -1,60 +1,30 @@
-# import sys
-# sys.path.append('/home/edvard/GitHub/Prediction')
-
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D  # anvands!
 import matplotlib.pyplot as plt
-from sklearn import neighbors
-from scipy.optimize import curve_fit
-from sklearn.externals import joblib
-import pickle
+from predictions.Estimators.LWRegressor import LWRegressor
 import cPickle
 
 data = np.load('data.npz')
 # untriageds = data['untriageds']
 seat_times = data['seat_times']
 weekdays = data['weekdays']
-daytimes = data['daytimes'] / 3600  # i timmar
-ttd = data['ttd']
+daytimes = data['daytimes'] / 60  # i minuter
+ttd = data['ttd'] / (1000 * 60)  # i minuter
+print ttd
 
-x1 = weekdays
-x2 = daytimes
-# X = np.column_stack([x1, x2])  # shape (870, 2)!
-X = x1 + 24 * x2
+x1 = weekdays.reshape(-1, 1)
+x2 = daytimes.reshape(-1, 1)
+X = x1 * 24 * 60 + x2
 y = ttd
 
-# plot av indata
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-ax.scatter(x1, x2, y, c='r', marker='o')
+xl = np.linspace(24 * 60, 24 * 8 * 60, 100).reshape(-1, 1)
 
-# # nnfit och plot av nnfit
-N = 100
-x1l = np.ones(N)
-x2l = np.linspace(0, 24, N)
-Xl = np.column_stack([x1l, x2l])  # shape (100, 2)
+lwReg = LWRegressor(sigma=120.0)
+lwReg.fit(X, y)
+y_pred = lwReg.predict(xl)
 
-xl = np.linspace(24 * 2, 24 * 7, N)  # cioe inte hela veckan
-
-#knn = neighbors.KNeighborsRegressor(n_neighbors=30, weights='uniform')
-rnn = neighbors.RadiusNeighborsRegressor(radius=20.1)#, weights='distance')
-X.shape = (861,1)
-y.shape = (861,1)
-print X.shape, y.shape
-#knn = knn.fit(X, y)
-rnn.fit(X,y)
-xl.shape = (N,1)
-print xl.shape
-#y_ = knn.predict(xl)
-y_ = rnn.predict(xl)
-
-ax2 = fig.add_subplot(122)
-ax2.plot(xl, y_)
-
+plt.scatter(X, y, marker='x')
+plt.plot(xl, y_pred, c='red')
 plt.show()
 
-# save the model
-# joblib.dump(knn, 'model.pkl')
-# pickle.dumps(knn)
-#with open('model.pkl', 'w') as file:
-#    cPickle.dump(knn, file)
+with open('model.pkl', 'w') as file:
+    cPickle.dump(lwReg, file)
